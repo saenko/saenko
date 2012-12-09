e7ad4013e03235dc1604a23b0ca760758f797477
Constantin Saenko <constantin.saenko@gmail.com>
2012-12-09 17:53:57 +0200

    домашка 8 (м2м дополнение)

---
+++ Project-Template/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+class newClass {
+	public var $myVar;
+
+	public function construct( $var, $var2, $var3 ) {
+
+	}
+}
+
+$newClass = new newClass();
+
+$newClass->myVar;
+
+static function geometricProgression( $a ) {
+
+	geometricProgression( $a * $a );	
+}
+
+geometricProgression( 2 );
+
+?>
